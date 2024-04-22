9cc2118fce34d8812acad0bfff52a3d2f5f7dc64
Damir Rysaev <freepad@ya.ru>
2024-04-22 20:49:54 +0300

    try use env in github

--- src/env.js
+++ src/env.js
@@ -1,5 +1,4 @@
-console.log('process.env.TEST_BASE_URL', process.env.TEST_BASE_URL)
-console.log('process.env.TEST_BASE_OTHER', process.env.TEST_BASE_OTHER)
+console.log('TEST_GITHUB_ENV', process.env.TEST_GITHUB_ENV)
 
 if (process.env.CI) {
   console.log('Запущено в github', process.env.CI)
