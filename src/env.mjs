3a5991a571d6aad487db040f9c880955cc49bbcb
Damir Rysaev <freepad@ya.ru>
2024-04-22 21:16:28 +0300

    try use env in github

--- src/env.mjs
+++ src/env.mjs
@@ -1,4 +1,8 @@
+import 'dotenv/config'
+
 console.log('TEST_GITHUB_ENV', process.env.TEST_GITHUB_ENV)
+console.log('TEST_OTHER', process.env.TEST_OTHER)
+console.log('TEST_PASSWORD', process.env.TEST_PASSWORD)
 
 if (process.env.CI) {
   console.log('Запущено в github', process.env.CI)
