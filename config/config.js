fdff30d0301cfd90a84a5d13509ebae6503e5ab0
qaemil2024 <169067069+qaemil2024@users.noreply.github.com>
2024-05-06 19:19:08 +0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -19,11 +19,11 @@ export const configPolicy = {
 }
 
 export const configCreateUser = {
-<<<<<<< HEAD
+
     "baseURL": "endpoint",
-=======
+
     "baseURL": "endpoint",
->>>>>>> e8177e7e811b9bb54ee7f2fa87e11a01abc20bc2
+
     "login": 'test',
     "password": 'test',
     "policy": ["3f07d273-4823-478f-82a4-4aa905aafbb1"],
